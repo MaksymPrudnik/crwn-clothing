707e0c6655fd6d5477e63d367935518778d32c6b
maksymprudnik <maksymprudnik@gmail.com>
2020-12-12 00:03:00 +0200

    init commit

---
+++ src/Homepage.component.jsx
@@ -0,0 +1,38 @@
+import './Homepage.styles.scss'
+
+export const Homepage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h2>Hats</h2>
+                    <span>Shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h2>Jackets</h2>
+                    <span>Shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h2>Sneakers</h2>
+                    <span>Shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h2>Mens</h2>
+                    <span>Shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h2>Womens</h2>
+                    <span>Shop now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
